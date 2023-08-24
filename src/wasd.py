6cb6223106af0dad3129c081482d5a61a98897bb
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2023-08-24 11:50:25 +0200

    Added debugging to the WASD controls and made input on release.

--- src/wasd.py
+++ src/wasd.py
@@ -3,26 +3,34 @@ import keyboard
 
 
 def WASD_control(tello: Tello, speed: int, rotation_speed: int, up_down_speed: int):
-    if keyboard.is_pressed('w'):
+    if keyboard.press_and_release('w'):
+        print('W')
         tello.move_forward(speed)
 
-    if keyboard.is_pressed('a'):
+    if keyboard.press_and_release('a'):
+        print('a')
         tello.rotate_counter_clockwise(rotation_speed)
 
-    if keyboard.is_pressed('s'):
+    if keyboard.press_and_release('s'):
+        print('s')
         tello.move_back(speed)
 
-    if keyboard.is_pressed('d'):
+    if keyboard.press_and_release('d'):
+        print('d')
         tello.rotate_clockwise(rotation_speed)
 
-    if keyboard.is_pressed('up arrow'):
+    if keyboard.press_and_release('up arrow'):
+        print('up arrow')
         tello.move_up(up_down_speed)
 
-    if keyboard.is_pressed('left arrow'):
+    if keyboard.press_and_release('left arrow'):
+        print('left arrow')
         tello.move_left(speed)
 
-    if keyboard.is_pressed('down arrow'):
+    if keyboard.press_and_release('down arrow'):
+        print('down arrow')
         tello.move_down(up_down_speed)
 
-    if keyboard.is_pressed('right arrow'):
+    if keyboard.press_and_release('right arrow'):
+        print('right arrow')
         tello.move_right(speed)
