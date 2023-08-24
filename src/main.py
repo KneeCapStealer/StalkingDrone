8f8b404f294ff77f89cdec417a584cd3cdc74dbc
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2023-08-24 13:22:17 +0200

    Fixed input, using Pygame

--- src/main.py
+++ src/main.py
@@ -1,28 +1,75 @@
 # Modules
 import cv2
+import pygame
 from djitellopy import Tello
-import keyboard
+import pygame as pg
 
 # Own Scripts
 import wasd
 
 
 def main():
-    MOVE_STEPS = 10
-    TURN_STEPS = 10
-    UP_DOWN_STEPS = 5
+    pg.init()
+    screen = pg.display.set_mode((720, 480), vsync=60)
+
+    MOVE_STEPS = 50
+    TURN_STEPS = 50
+    UP_DOWN_STEPS = 50
 
     tello = Tello()
+    tello.connect()
+    tello.takeoff()
+
+    print(tello.get_battery())
 
     # Main loop
     running = True
     while running:
-        wasd.WASD_control(tello, MOVE_STEPS, TURN_STEPS, UP_DOWN_STEPS)  # Control drone using WASD and arrow keys
+        keys_pressed = {
+            'w': False,
+            'a': False,
+            's': False,
+            'd': False,
+            'up arrow': False,
+            'down arrow': False,
+            'left arrow': False,
+            'right arrow': False,
+        }
 
-        if keyboard.is_pressed('esc'):
-            running = False
+        events = pg.event.get()
+        for event in events:
+            if event.type == pg.QUIT:
+                running = False
+
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    running = False
+
+            elif event.type == pygame.KEYUP:
+                match event.key:
+                    case pg.K_w:
+                        keys_pressed['w'] = True
+                    case pg.K_a:
+                        keys_pressed['a'] = True
+                    case pg.K_s:
+                        keys_pressed['s'] = True
+                    case pg.K_d:
+                        keys_pressed['d'] = True
+                    case pg.K_UP:
+                        keys_pressed['up arrow'] = True
+                    case pg.K_DOWN:
+                        keys_pressed['down arrow'] = True
+                    case pg.K_RIGHT:
+                        keys_pressed['right arrow'] = True
+                    case pg.K_LEFT:
+                        keys_pressed['left arrow'] = True
+
+        wasd.WASD_control(tello, keys_pressed, MOVE_STEPS, TURN_STEPS, UP_DOWN_STEPS)
+
+    tello.land()
 
 
 if __name__ == "__main__":
     main()
+    pg.quit()
     quit('Exit_Success')
--- src/wasd.py
+++ src/wasd.py
@@ -1,36 +1,36 @@
 from djitellopy import Tello
-import keyboard
+import pygame
 
 
-def WASD_control(tello: Tello, speed: int, rotation_speed: int, up_down_speed: int):
-    if keyboard.press_and_release('w'):
+def WASD_control(tello: Tello, keysPressed: dict[str, bool], speed: int, rotation_speed: int, up_down_speed: int):
+    if keysPressed['w']:
         print('W')
         tello.move_forward(speed)
 
-    if keyboard.press_and_release('a'):
+    if keysPressed['a']:
         print('a')
         tello.rotate_counter_clockwise(rotation_speed)
 
-    if keyboard.press_and_release('s'):
+    if keysPressed['s']:
         print('s')
         tello.move_back(speed)
 
-    if keyboard.press_and_release('d'):
+    if keysPressed['d']:
         print('d')
         tello.rotate_clockwise(rotation_speed)
 
-    if keyboard.press_and_release('up arrow'):
+    if keysPressed['up arrow']:
         print('up arrow')
         tello.move_up(up_down_speed)
 
-    if keyboard.press_and_release('left arrow'):
+    if keysPressed['left arrow']:
         print('left arrow')
         tello.move_left(speed)
 
-    if keyboard.press_and_release('down arrow'):
+    if keysPressed['down arrow']:
         print('down arrow')
         tello.move_down(up_down_speed)
 
-    if keyboard.press_and_release('right arrow'):
+    if keysPressed['right arrow']:
         print('right arrow')
         tello.move_right(speed)
