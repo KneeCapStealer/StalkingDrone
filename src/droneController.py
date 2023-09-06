812ab7b582a79be1345226c49013ebc5012c1449
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2023-09-06 15:33:51 +0200

    Started threads in init and stopping them in del while making 2 functions to start tracking and wasd controls

--- src/droneController.py
+++ src/droneController.py
@@ -1,8 +1,8 @@
 import custom_threads
 import threading
 
-class DroneController:
 
+class DroneController:
     class TrackingData:
         def __init__(self):
             self.x: float = 0
@@ -10,22 +10,39 @@ class DroneController:
             self.w: float = 0
             self.h: float = 0
 
-
-
     def __init__(self):
         self.tracking_data: DroneController.TrackingData = DroneController.TrackingData()
         self.lock = threading.Lock()
-        self.tracking_thread = custom_threads.LoopThread(self._tracking_func)
-        self.WASD_thread = custom_threads.LoopThread(self._WASD_func)
+        self.tracking_thread = custom_threads.LoopThread(self._tracking_control)
+        self.WASD_thread = custom_threads.LoopThread(self._WASD_control)
 
-    def WASD_control(self):
+        self.tracking_thread.daemon = True
+        self.WASD_thread.daemon = True
+
+        self.tracking_thread.pause(True)
+        self.WASD_thread.pause(True)
+
+        self.tracking_thread.start()
+        self.WASD_thread.start()
+
+    def __del__(self):
+        self.tracking_thread.stop()
+        self.WASD_thread.stop()
+
+    def start_WASD(self):
+        self.tracking_thread.pause(True)
+        self.WASD_thread.pause(False)
+
+    def start_tracking(self):
+        self.WASD_thread.pause(True)
+        self.tracking_thread.pause(False)
+
+    def _tracking_control(self):
         pass
 
-    def tracking_control(self):
+    def _track_humans(self):
         pass
 
-    def _tracking_func(self):
+    def _WASD_control(self):
         pass
 
-    def _WASD_func(self):
-        pass
